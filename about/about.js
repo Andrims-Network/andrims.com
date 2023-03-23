99ba57a3eda215cf9f0d849b5fbb446aba4fbd89
HalfCookedRamen <75753005+the-ramenator@users.noreply.github.com>
2023-03-23 18:54:06 +0000

    update to the about effect

--- about/about.js
+++ about/about.js
@@ -5,7 +5,7 @@ function random(min, max, fixed) {
 function newComet(){
   this.obj = document.createElement("div");
   this.obj.classList.add("comet");
-  this.obj.style.top = ((window.innerHeight * Math.random())/2)+'px';
+  this.obj.style.top = (window.innerHeight * Math.random())+'px';
   this.obj.style.left = (window.innerWidth * Math.random()) + 'px';
   this.obj.style.animation = "comet " + random(5, 10, 0) + "s linear infinite";
   this.obj.style.animationDelay = random(0, 2, 2) + 's';
@@ -15,6 +15,16 @@ function newComet(){
   document.getElementById('comet-container').appendChild(this.obj);
 }
 var comets = [];
-for (var i = 0; i < random(20, 100, 0); i++) {
+for (var i = 0; i < random(200,300, 0); i++) {
   comets.push(new newComet());
 }
+
+const bg = document.getElementById('comet-container');
+const windowWidth = 50 + (window.innerWidth / 5);
+const windowHeight = 50 + (window.innerHeight / 5);
+
+document.body.addEventListener("mousemove", function(e) {
+  const mouseX = e.clientX / windowWidth;
+  const mouseY = e.clientY / windowHeight;
+  bg.style.transform = `translate3d(-${mouseX}%, -${mouseY}%, 0)`;
+});
